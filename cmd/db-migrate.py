f3c65116e50b62d51ef2cd2ceffad12dbe6f3139
Tomek Mrugalski <thomson@klub.com.pl>
2020-08-22 22:07:29 +0200

    fwhm, eccentricity fields added

--- cmd/db-migrate.py
+++ cmd/db-migrate.py
@@ -4,6 +4,8 @@
 import sys
 sys.path.append("e:\\devel\\hevelius-proc")
 
+import pprint
+
 from hevelius import db
 
 print("Hevelius db-migrate 0.1")
@@ -11,5 +13,28 @@ print("Hevelius db-migrate 0.1")
 cnx = db.connect()
 
 v = db.version_get(cnx)
+
+print("Schema version is %d" % v)
+
+stats = db.stats_get(cnx)
+print("There are %d tasks, %d files." % stats)
+
+print("Stats by state:")
+by_state = db.stats_by_state(cnx)
+print(by_state)
+
+print("Status of task 133989: BEFORE")
+t1 = db.task_get(cnx, 133989)
+print(t1)
+
+db.task_update(cnx, 133989, fwhm = 12.34, eccentricity=54.321)
+
+print("Status of task 133989: AFTER")
+t1 = db.task_get(cnx, 133989)
+print(t1)
+
+
+#t2 = db.task_get(cnx, 133910)
+#print(t2)
 
 cnx.close()
--- hevelius/db.py
+++ hevelius/db.py
@@ -7,6 +7,17 @@ def connect():
     cnx = mysql.connector.connect(user=config.USER, password=config.PASSWORD, database=config.DBNAME, host=config.HOST, port=config.PORT)
     return cnx
 
+def run_query(cnx, query):
+    cursor = cnx.cursor(buffered=True) # cursor(dictionary=True) or cursor(named_tuple=True)
+    cursor.execute(query)
+    print("#### rowcount = %d" % cursor.rowcount)
+    try:
+        result = cursor.fetchall()
+    except:
+        result = None    
+    cursor.close()
+    return result
+
 def version_get(cnx):
     query = 'SELECT * from schema_version'
     cursor = cnx.cursor()
@@ -16,5 +27,77 @@ def version_get(cnx):
         ver = i[0]
 
     cursor.close()
-    print("Schema version detected %d" % ver)
     return ver
+
+def stats_get(cnx):
+
+    # Get overall tasks counters
+    tasks_cnt = run_query(cnx, 'SELECT count(*) from tasks')
+    files_cnt = run_query(cnx, 'SELECT count(*) from tasks where imagename is not null')
+
+    return tasks_cnt[0][0], files_cnt[0][0]
+
+def stats_by_state(cnx):
+    # Get tasks list by status
+    hist = run_query(cnx, 'SELECT state, count(*) from tasks group by state')
+    res = []
+
+    for row in hist:
+        res.append( (row[0], row[1]))
+
+    return res
+
+def stats_by_user(cnx, state = 6):
+    if state is None:
+        cond = ""
+    else:
+        cond = "WHERE state == %d" % state
+
+    q = "SELECT login, tasks.user_id, count(*) from tasks, users where tasks.user_id = users.user_id %s group by tasks.user_id" % cond
+
+    tasks_per_user = run_query(cnx, q)
+
+    return tasks_per_user
+
+def task_get(cnx, id):
+    q = "SELECT task_id, state, user_id, imagename, object, descr, comment, ra, decl, exposure, filter, binning, guiding, fwhm, eccentricity FROM tasks WHERE task_id = %d" % id
+
+    t = run_query(cnx, q)[0]
+
+    x = {}
+    x["id"] = t[0]
+    x["state"] = t[1]
+    x["user_id"] = t[2]
+    x["file"] = t[3]
+    x["object"] = t[4]
+    x["descr"] = t[5]
+    x["comment"] = t[6]
+    x["ra"] = t[7]
+    x["decl"] = t[8]
+    x["exposure"] = t[9]
+    x["filter"] = t[10]
+    x["binning"] = t[11]
+    x["guiding"] = t[12]
+    x["fwhm"] = t[13]
+    x["eccentricity"] = t[14]
+
+    return x
+
+def task_update(cnx, id, fwhm = None, eccentricity = None):
+    upd = ""
+    if fwhm is not None:
+        upd = "fwhm = %f" % fwhm
+    if eccentricity is not None:
+        if len(upd):
+            upd += ", "
+        upd += "eccentricity = %f" % eccentricity
+    
+    if not len(upd):
+        print("Nothing to update in task %d, aborting" % id)
+
+    q = "UPDATE tasks SET %s WHERE task_id=%d" % (upd, id)
+
+    print("#### query=[%s]" % q)
+
+    run_query(cnx, q)
+        
