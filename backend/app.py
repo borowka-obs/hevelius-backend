02788b93a0e3b316ac260db03425e2c13efe84b4
Tomek Mrugalski <tomek@isc.org>
2023-01-01 23:56:22 +0100

    API /api/login now implemented and working!

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 import pandas as pd
 import json
 import plotly
@@ -9,6 +10,7 @@ from hevelius import db_mysql as db
 # By default, Flask searches for templates in the templates/ dir.
 # Other params: debug=True, port=8080
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def root():
@@ -27,7 +29,7 @@ def histogram():
    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
    return render_template('histogram.html', graphJSON=graphJSON)
 
-@app.route('/api/tasks/')
+@app.route('/api/tasks')
 def tasks():
 
     cnx = db.connect()
@@ -42,3 +44,84 @@ def tasks():
     }]
 
     return tasks
+
+
+def sanitize(x: str) -> str:
+    x = x.replace('\'','') # apostrophes are bad
+    x = x.replace(';','') # commas also
+    x = x.replace('\\','') # and backslashes
+    return x
+
+def get_param(request, field) -> str:
+    json = request.get_json()
+    x = json.get(field)
+    if x:
+        return sanitize(x)
+    return x
+
+@app.route('/api/login', methods = ['POST'])
+@cross_origin()
+def login():
+
+    """_summary_
+
+    Example request:    {"username":"tomek","password":"64a4e327e97c1e7926f9240edb123456"}
+
+    Example response:
+    {
+        "aavso_id":"MTOA",
+        "email":"some.mail@example.org",
+        "firstname":"Tomek",
+        "ftp_login":"MTOA",
+        "ftp_pass":"xxxxx",
+        "lastname":"M.",
+        "msg":"Welcome",
+        "permissions":1,
+        "phone":"",
+        "share":0.0,
+        "status":true,
+        "user_id":3
+    }
+
+    :return: _description_
+    :rtype: _type_
+    """
+
+    if not request.is_json:
+        return { 'status': False, 'msg': 'Need JSON input' }
+
+    user = get_param(request, 'username')
+    md5pass = get_param(request, 'password')
+
+    if user is None:
+        return { 'status': False, 'msg': 'Username not provided'}
+    if md5pass is None:
+        return { 'status': False, 'msg': 'Password not provided'}
+
+    q = f"SELECT user_id, pass_d, login, firstname, lastname, share, phone, email, permissions, aavso_id, ftp_login, ftp_pass FROM users WHERE login='{user}'"
+
+    cnx = db.connect()
+    db_resp = db.run_query(cnx, q)
+    cnx.close()
+
+    if db_resp is None or not len(db_resp):
+        print("#### No username")
+        return { 'status': False, 'msg': 'Invalid credentials'} # No such username
+
+    user_id, pass_db, login, firstname, lastname, share, phone, email, permissions, aavso_id, ftp_login, ftp_pass = db_resp[0]
+    if (md5pass.lower() != pass_db.lower()):
+        print(f"#### Login exists, but invalid password: expected {pass_db.lower()}, got {md5pass.lower()}")
+        return { 'status': False, 'msg': 'Invalid credentials'} # Password's MD5 did not match
+
+    return { 'status': True,
+             'user_id': user_id,
+             'firstname': firstname,
+             'lastname': lastname,
+             'share': share,
+             'phone': phone,
+             'email': email,
+             'permissions': permissions,
+             'aavso_id': aavso_id,
+             'ftp_login': ftp_login,
+             'ftp_pass': ftp_pass,
+             'msg': 'Welcome' }
---
+++ doc/backend.md
@@ -0,0 +1,6 @@
+To run backend locally, run:
+
+```shell
+cd backend/
+python3 -m flask run
+```
