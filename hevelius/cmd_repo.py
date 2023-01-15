0c5b834d63e0b5d75cc907bc12246ac7b7ff2b9f
Tomek Mrugalski <tomek@isc.org>
2023-01-16 00:39:18 +0100

    parse_iteleskop_filename moved to the right place

--- hevelius/cmd_repo.py
+++ hevelius/cmd_repo.py
@@ -1,42 +1,6 @@
 from hevelius import config, db
+from hevelius.iteleskop import parse_iteleskop_filename
 import glob
-import re
-import os
-
-
-def parse_iteleskop_filename(fname):
-
-    # Step 1: get rid of the possible leading dir name.
-    dir, filename = os.path.split(fname)  # get rid of the directory
-    filename, _ = os.path.splitext(filename) # and the extension
-
-    regex = '([S_][F_][D_][B_])_([0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2})_J([0-9]+)_([A-Z]+)_([A-Z0-9]+)_([0-9]x[0-9])_([0-9]+)s_(.*)'
-    m = re.match(regex, filename)
-    if m:
-        flags, date, task_id, user,filter, bin, exp, object = m.groups()
-        bin = int(bin[0])
-
-        solved = (flags[0] == 'S')
-        calibrated = (flags[1:4] == "FDB")
-
-        # print(f"Parsed flags={flags} date={date} task_id={task_id} user={user} filter={filter} bin={bin} exp={exp} object={object}")
-        return {
-            "flags": flags,
-            "date": date,
-            "task_id": int(task_id),
-            "user": user,
-            "filter": filter,
-            "binning": int(bin),
-            "exposure": exp,
-            "object": object,
-            "imagename": fname,
-            "solve": solved,
-            "solved": solved,
-            "calibrate": calibrated,
-            "calibrated": calibrated
-        }
-    else:
-        return None
 
 def task_add(cnx, fname, details):
     """Adds a new task based on a image filename, specified by fname. The
@@ -51,6 +15,8 @@ def task_add(cnx, fname, details):
     details["state"] = 6 # Complete
 
     print(details)
+
+    # TODO: load fits, get its parameters. For example code, see cmd/parse-fits.py
 
     db.task_add(cnx, details)
 
