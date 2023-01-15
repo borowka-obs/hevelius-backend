c85dc7a79568b86b10b746ba40fd09a82764a6a2
Tomek Mrugalski <tomek@isc.org>
2023-01-15 21:27:59 +0100

    First test added

---
+++ tests/test_config.py
@@ -0,0 +1,15 @@
+from hevelius import config
+import unittest
+
+class ConfigTest(unittest.TestCase):
+
+    def test_variables(self):
+        """Checks if hevelius.config defines necessary configuration parameters."""
+        self.assertTrue(config.USER and type(config.USER) == str)
+        self.assertTrue(config.DBNAME and type(config.USER) == str)
+        self.assertTrue(config.PASSWORD and type(config.USER) == str)
+        self.assertTrue(config.PORT and type(config.PORT) == int)
+        self.assertTrue(config.TYPE and type(config.TYPE) == str and config.TYPE in ['mysql', 'pgsql'])
+
+        # Uncomment this once REPO_PATH is implemented
+        # self.assertTrue(config.REPO_PATH and type(config.USER) == str)
