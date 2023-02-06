fea33fe99bda560a76931f3a6ec58b1690b22bde
Tomek Mrugalski <tomek@isc.org>
2023-02-06 23:53:50 +0100

    backup command added

--- hevelius/cmd_basic.py
+++ hevelius/cmd_basic.py
@@ -4,7 +4,10 @@ Code handling several basic commands (stats, version, config)
 
 from importlib.metadata import version as importlib_version
 from hevelius import db, config
-
+import datetime
+import subprocess
+import pathlib
+from os import path
 
 def db_version():
     """
@@ -50,3 +53,26 @@ def config_show():
     print(f"Database: {config.DBNAME}")
     print(f"Host:     {config.HOST}")
     print(f"Port:     {config.PORT}")
+
+    print()
+
+    print("Files repository path: {config.REPO_PATH}")
+    print("Backup storage path:   {config.BACKUP_PATH}")
+
+def backup(args):
+    """
+    Generated DB backup
+    """
+
+    backup_name = datetime.datetime.now().strftime("hevelius-backup-%Y-%m-%d-%H-%M-%S.psql")
+
+    full_path = path.join(config.BACKUP_PATH, backup_name)
+
+    pathlib.Path(config.BACKUP_PATH).mkdir(parents=True, exist_ok=True)
+
+    psql = subprocess.Popen(["pg_dump", "-U", config.USER, "-h", config.HOST, "-p",
+                                str(config.PORT), config.DBNAME, "-f", full_path ])
+    # this returns std output, (something else)
+    output, _ = psql.communicate()
+
+    print(f"Backup stored in {full_path}")
