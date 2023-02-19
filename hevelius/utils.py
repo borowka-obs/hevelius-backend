659d09578eedba974d39bd20bb02d237c8219d98
Tomek Mrugalski <tomek@isc.org>
2023-02-19 22:26:57 +0100

    Constellation table + funcs added

--- hevelius/utils.py
+++ hevelius/utils.py
@@ -1,3 +1,118 @@
+
+CONSTELLATION_NAMES = {
+        "And": "Andromeda",
+        "Ant": "Antlia",
+        "Aps": "Apus",
+        "Aqr": "Aquarius",
+        "Aql": "Aquila",
+        "Ara": "Ara",
+        "Ari": "Aries",
+        "Aur": "Auriga",
+        "Boo": "Boötes",
+        "Cae": "Caelum",
+        "Cam": "Camelopardalis",
+        "Cnc": "Cancer",
+        "Cvn": "Canes Venatici",
+        "CMa": "Canis Major",
+        "CMi": "Canis Minor",
+        "Cap": "Capricornus",
+        "Car": "Carina",
+        "Cas": "Cassiopeia",
+        "Cen": "Centaurus",
+        "Cep": "Cepheus",
+        "Cet": "Cetus",
+        "Cha": "Chamaeleon",
+        "Cir": "Circinus",
+        "Col": "Columba",
+        "Com": "Coma Berenices",
+        "CrA": "Corona Australis",
+        "CrB": "Corona Borealis",
+        "Crv": "Corvus",
+        "Crt": "Crater",
+        "Cru": "Crux",
+        "Cyg": "Cygnus",
+        "Del": "Delphinus",
+        "Dor": "Dorado",
+        "Dra": "Draco",
+        "Equ": "Equuleus",
+        "Eri": "Eridanus",
+        "For": "Fornax",
+        "Gem": "Gemini",
+        "Gru": "Grus",
+        "Her": "Hercules",
+        "Hor": "Horologium",
+        "Hya": "Hydra",
+        "Hyi": "Hydrus",
+        "Ind": "Indus",
+        "Lac": "Lacerta",
+        "Leo": "Leo",
+        "LMi": "Leo Minor",
+        "Lep": "Lepus",
+        "Lib": "Libra",
+        "Lup": "Lupus",
+        "Lyn": "Lynx",
+        "Lyr": "Lyra",
+        "Men": "Mensa",
+        "Mic": "Microscopium",
+        "Mon": "Monoceros",
+        "Mus": "Musca",
+        "Nor": "Norma",
+        "Oct": "Octans",
+        "Oph": "Ophiuchus",
+        "Ori": "Orion",
+        "Oav": "Pavo",
+        "Peg": "Pegasus",
+        "Per": "Perseus",
+        "Phe": "Phoenix",
+        "Pic": "Pictor",
+        "Psc": "Pisces",
+        "PsA": "Piscis Austrinus",
+        "Pup": "Puppis",
+        "Pyx": "Pyxis",
+        "Ret": "Reticulum",
+        "Sge": "Sagitta",
+        "Sgr": "Sagittarius",
+        "Sco": "Scorpius",
+        "Scl": "Sculptor",
+        "Sct": "Scutum",
+        "Ser": "Serpens",
+        "Sex": "Sextans",
+        "Tau": "Taurus",
+        "Tel": "Telescopium",
+        "Tri": "Triangulum",
+        "TrA": "Triangulum Australe",
+        "Tuc": "Tucana",
+        "UMa": "Ursa Major",
+        "UMi": "Ursa Minor",
+        "Vel": "Vela",
+        "Vir": "Virgo",
+        "Vol": "Volans",
+        "Vul": "Vulpecula"
+    }
+
+def constellation2abbrev(cons:str) -> str:
+    """
+    Provides Abbreviation for a full constellation name
+    """
+
+    for key in CONSTELLATION_NAMES:
+        if cons.lower() == CONSTELLATION_NAMES[key].lower():
+            return key
+
+    raise ValueError(f"Constellation {cons} not found")
+
+
+def abbrev2constellation(cons: str) -> str:
+    """
+    Provides full constellation name for an abbreviation
+    """
+
+    if cons in CONSTELLATION_NAMES:
+        return CONSTELLATION_NAMES[cons]
+
+    raise ValueError(f"Constellation {cons} not found")
+
+
 def deg2rah(ra: float) -> str:
     """Converts Right Ascension specified in degrees (0..359) to hour
     (0..23.59)"""
