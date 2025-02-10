56073a1747cca59377313ade6a96f984ab0b48c3
Tomek Mrugalski <tomek@isc.org>
2025-02-10 21:10:23 +0100

    Config mgmt rewritten

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,4 @@
-from hevelius import config
+from hevelius import config as hevelius_config
 import unittest
 
 
@@ -6,11 +6,14 @@ class ConfigTest(unittest.TestCase):
 
     def test_variables(self):
         """Checks if hevelius.config defines necessary configuration parameters."""
-        self.assertTrue(config.USER and isinstance(config.USER, str))
-        self.assertTrue(config.DBNAME and isinstance(config.USER, str))
-        self.assertTrue(config.PASSWORD and isinstance(config.USER, str))
-        self.assertTrue(config.PORT and isinstance(config.PORT, int))
-        self.assertTrue(config.TYPE and isinstance(config.TYPE, str) and config.TYPE in ['mysql', 'pgsql'])
+
+        cfg = hevelius_config.load_config()
+
+        self.assertTrue(cfg['DATABASE']['USER'] and isinstance(cfg['DATABASE']['USER'], str))
+        self.assertTrue(cfg['DATABASE']['DBNAME'] and isinstance(cfg['DATABASE']['USER'], str))
+        self.assertTrue(cfg['DATABASE']['PASSWORD'] and isinstance(cfg['DATABASE']['USER'], str))
+        self.assertTrue(cfg['DATABASE']['PORT'] and isinstance(cfg['DATABASE']['PORT'], int))
+        self.assertTrue(cfg['DATABASE']['TYPE'] and isinstance(cfg['DATABASE']['TYPE'], str) and cfg['DATABASE']['TYPE'] in ['mysql', 'pgsql'])
 
         # Uncomment this once REPO_PATH is implemented
         # self.assertTrue(config.REPO_PATH and type(config.USER) == str)
