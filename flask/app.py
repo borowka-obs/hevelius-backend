3c914cad162299cebc510cf977d9558e38611fd6
Tomek Mrugalski <tomek@isc.org>
2025-02-11 22:59:15 +0100

    Fix for the /login API call

--- flask/app.py
+++ flask/app.py
@@ -133,11 +133,12 @@ def login():
     if md5pass is None:
         return {'status': False, 'msg': 'Password not provided'}
 
-    query = f"""SELECT user_id, pass_d, login, firstname, lastname, share, phone, email, permissions,
-            aavso_id, ftp_login, ftp_pass FROM users WHERE login='{user}'"""
+    query = """SELECT user_id, pass_d, login, firstname, lastname, share, phone, email, permissions,
+            aavso_id, ftp_login, ftp_pass FROM users WHERE login=%s"""
+    params = [user]
 
     cnx = db.connect()
-    db_resp = db.run_query(cnx, query)
+    db_resp = db.run_query(cnx, query, params)
     cnx.close()
 
     if db_resp is None or not len(db_resp):
