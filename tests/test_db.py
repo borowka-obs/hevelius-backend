ec863e54e74e30a9d759eaf1e438f3365543459d
Tomek Mrugalski <thomson@klub.com.pl>
2025-04-16 19:09:43 -0500

    Schema bumped to 13
     - new fields for scopes
     - relation between scopes and sensors added

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,7 +13,7 @@ class DbTest(unittest.TestCase):
         version = db.version_get(conn)
         conn.close()
 
-        self.assertEqual(version, 12)
+        self.assertEqual(version, 13)
 
     @use_repository
     def test_sensor(self, config):
