257621dacb2b3e47f6916740a2188ccdd455dd11
Tomek Mrugalski <tomek@isc.org>
2026-02-22 21:00:14 +0100

    Initial support for asteroids added

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,7 +13,7 @@ class DbTest(unittest.TestCase):
         version = db.version_get(conn)
         conn.close()
 
-        self.assertEqual(version, 14)
+        self.assertEqual(version, 15)
 
     @use_repository
     def test_sensor(self, config):
