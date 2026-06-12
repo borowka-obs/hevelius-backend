749b1e2a5135252d9785582ef65e4a6ecf9b5ffb
Tomek Mrugalski <thomson@klub.com.pl>
2026-06-12 23:31:48 +0200

    Tests for versions fixed

--- tests/test_db.py
+++ tests/test_db.py
@@ -28,7 +28,7 @@ class DbTest(unittest.TestCase):
         version = db.version_get(conn)
         conn.close()
 
-        self.assertEqual(version, 21)
+        self.assertEqual(version, 22)
 
     @use_repository
     def test_sensor(self, config):
