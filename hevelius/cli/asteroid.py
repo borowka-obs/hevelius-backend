bbd25e869010764aaa081629bbec01e39549cddd
Tomek Mrugalski <thomson@klub.com.pl>
2026-07-24 00:26:20 +0200

    flake8 appeased

--- hevelius/cli/asteroid.py
+++
@@ -1,2 +0,0 @@
-"""CLI entry points for asteroid commands (logic lives in hevelius.asteroid)."""
-from hevelius.asteroid import *  # pylint: disable=wildcard-import,unused-wildcard-import
--- hevelius/cli/equipment.py
+++
@@ -1,2 +0,0 @@
-"""CLI wrappers for equipment management (logic lives in hevelius.equipment)."""
-from hevelius.equipment import *  # pylint: disable=wildcard-import,unused-wildcard-import
